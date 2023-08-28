169ebac1d023a40ece2216a516debfc1561db599
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-08-28 22:41:04 +0300

    commit

--- file_writer.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open ('library.csv' , 'w', newline = '') as file:
-    fieldnames = ['Book name', 'Author', 'Release year', 'Quantity']
-    writer = csv.DictWriter(file, fieldnames = fieldnames)
-    
-    writer.writeheader()
